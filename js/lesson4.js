3b833dbb3385410c215240c9e48ab563a09ecabb
Mirko40 <mmirko@ua.fm>
2022-08-09 22:06:04 +0300

    Initial

---
+++ js/lesson4.js
@@ -0,0 +1,25 @@
+"use strict"
+
+function getСomparison(a, b) {
+    let numOne = a;
+    let numTwo = b;
+
+    if (numOne < numTwo) {
+        let result = -1;
+        console.log(`${result}`);
+    } else if (numOne > numTwo) {
+        let result = 1;
+        console.log(`${result}`);
+    } else if (numOne == numTwo) {
+        let result = 0;
+        console.log(`${result}`);
+    };
+};
+
+
+
+
+
+
+
+
