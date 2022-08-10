b479e68784d0e94c7d018ec3e1bd0810212cb28a
Mirko40 <mmirko@ua.fm>
2022-08-10 22:09:41 +0300

    Initial

--- js/lesson4.js
+++ js/lesson4.js
@@ -1,36 +1,26 @@
 "use strict"
 
 function getСomparison(a, b) {
-    let numOne = a;
-    let numTwo = b;
-
-    if (numOne < numTwo) {
-        let result = -1;
-        console.log(`${result}`);
-    } else if (numOne > numTwo) {
-        let result = 1;
-        console.log(`${result}`);
-    } else if (numOne == numTwo) {
-        let result = 0;
-        console.log(`${result}`);
-    };
+    if (a < b) {
+        return -1;
+    } else if (a > b) {
+        return 1;
+    } else return 0;
 };
 
 function getFactorial(n) {
     let i = 0;
     let result = 1;
-    let factorN = n;
-    if (factorN > 0) {
-        while (factorN > i) {
-
+    if (n >= 0) {
+        while (n > i) {
             result = result * (n - i);
             i++;
-
         };
-        console.log(`${result}`);
-    }
-    else console.log(`Number >= 0`);
+        return result;
+    } else console.log(`Number < 0`);
 };
+
+
 
 
 
